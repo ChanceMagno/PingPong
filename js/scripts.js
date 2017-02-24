2f786cb39944c4755ea2853bb941b0d43f6c69ef
cm <chancemagno@gmail.com>
2017-02-24 13:12:38 -0800

    add styles to header, margin to ul, adjust input size

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,14 @@
 $(document).ready(function () {
   $("#pingPong").submit(function(event){
     $("#pingPongOutput").empty();
-    event.preventDefault();
-
-
     var userInput = $("#userInput").val();
+    event.preventDefault();
 
      for (number = 1; number <= userInput; number++) {
        if ((number % 15) === 0) {
-          $("#pingPongOutput").append("<li>"+"PingPong"+"</li>");
+          $("#pingPongOutput").append("<li>"+"Ping Pong"+"</li>");
        } else if ((number % 5) === 0) {
-          $("#pingPongOutput").append("<li>"+"Pong"+"</li>");;
+          $("#pingPongOutput").append("<li>"+"Pong"+"</li>");
        } else if ((number % 3) === 0) {
           $("#pingPongOutput").append("<li>"+"Ping"+"</li>");
        } else {
