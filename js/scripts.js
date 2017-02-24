7c7c75e18ee5f2d286326b138f850599a84f39a5
cm <chancemagno@gmail.com>
2017-02-24 10:20:07 -0800

    add ability to get userInput value, crrected form type to numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
   $("#pingPong").submit(function(event){
-    
+    event.preventDefault();
+    var userInput = $(parseInt("#userInput")).val();
+    if ( userInput = 0) {
+      alert("not a number")
+    } else {alert("didnt work")}
   });
 });
